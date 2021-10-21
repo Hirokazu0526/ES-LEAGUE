b52fc5591b8edbf01af9f7e3c681f044a02752f8
Hirokazu0526 <sepak.hirokazu10@gmail.com>
2021-10-22 01:31:30 +0900

    add atoms components

---
+++ src/components/integration/atoms/es-button.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="button">
+    <nuxt-link
+      :to="url"
+      :class="{ color, arrow: arrow, border: border }"
+      :style="{ height: `${height}px` }"
+    >
+      <slot />
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    color: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    border: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    arrow: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    height: {
+      type: Number,
+      required: false,
+      default: 38,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  text-align: center;
+  width: 120px;
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 38px;
+    position: relative;
+    margin: 0 auto;
+    background-color: #344152;
+    border-radius: 8px;
+    color: white;
+    font-size: 14px;
+    font-weight: bold;
+    text-align: center;
+    text-decoration: none;
+    &:hover {
+      color: #fff;
+    }
+    &:after {
+      content: '>';
+      width: 14px;
+      height: 14px;
+      position: absolute;
+      top: 0px;
+      right: 10px;
+      bottom: 0px;
+      margin: auto;
+      margin-left: 5px;
+      font-size: 14px;
+      line-height: 14px;
+    }
+    &.border {
+      border: 1px solid #3895ff;
+    }
+    &.white {
+      color: rgb(28, 38, 61);
+      background-color: #fff;
+    }
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
+</style>
---
+++ src/components/integration/atoms/humberger.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <div
+      class="burger-btn"
+      :class="{ 'is-active': open }"
+      @click="open = !open"
+    >
+      <template v-if="!open">
+        <span class="bar bar_top--open"></span>
+        <span class="bar bar_mid--open"></span>
+        <span class="bar bar_bottom--open"></span>
+      </template>
+      <template v-else>
+        <span class="bar--close">×</span>
+      </template>
+    </div>
+    <div class="menu" :class="{ 'is-active': open }">
+      <div class="menu__item">TOP</div>
+      <div class="menu__item">ABOUT</div>
+      <div class="menu__item">BLOG</div>
+      <div class="menu__item">CONTACT</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.burger-btn {
+  display: block;
+  width: 39px;
+  height: 39px;
+  position: relative;
+  z-index: 3;
+  border: none;
+}
+.bar {
+  width: 20px;
+  height: 1px;
+  display: block;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #fff;
+  &_top--open {
+    top: 10px;
+  }
+  &_mid--open {
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+  &_bottom--open {
+    bottom: 10px;
+  }
+  &--close {
+    display: block;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: 200px;
+    font-weight: bold;
+    color: #fff;
+  }
+}
+.menu {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 1;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: #555;
+}
+.menu__item {
+  width: 100%;
+  height: auto;
+  padding: 0.5em 1em;
+  text-align: center;
+  color: #fff;
+  box-sizing: border-box;
+}
+
+/*----------------------------
+* アニメーション部分
+*----------------------------*/
+
+/* アニメーション前のメニューの状態 */
+.menu {
+  transform: translateX(100vw);
+  transition: all 0.3s linear;
+}
+/* アニメーション後のメニューの状態 */
+.menu.is-active {
+  transform: translateX(0);
+}
+</style>
