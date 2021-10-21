751efc9752a79a4f352306d0a7e10d5a8e7352a1
Hirokazu0526 <sepak.hirokazu10@gmail.com>
2021-10-22 01:31:59 +0900

    add layout of first commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="header">
+      header
+      <humberger class="header__btn" />
+    </div>
+
+    <Nuxt />
+    <div class="footer">footer</div>
+  </div>
+</template>
+
+<script>
+import humberger from '~/components/integration/atoms/humberger.vue'
+export default {
+  components: {
+    humberger,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  height: 80px;
+  background-color: rgb(87, 87, 87);
+  position: relative;
+  &__btn {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    display: block;
+  }
+}
+.footer {
+  height: 80px;
+  background-color: rgb(87, 87, 87);
+}
+</style>
