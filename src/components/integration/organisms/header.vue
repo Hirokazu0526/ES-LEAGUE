31f123c6f9d7baea25d34e4dfa71202b2cd13773
Hirokazu0526 <sepak.hirokazu10@gmail.com>
2021-10-26 00:33:54 +0900

    add header.vue

---
+++ src/components/integration/organisms/header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header">
+    <nuxt-link to="/">
+      <img
+        id="logo"
+        class="header__logo"
+        src="~/assets/img/site-logo.png"
+        alt="ロゴ"
+      />
+    </nuxt-link>
+    <global-nav />
+    <humberger class="header__btn" />
+  </div>
+</template>
+
+<script>
+import humberger from '~/components/integration/atoms/humberger.vue'
+import globalNav from '~/components/integration/organisms/global-nav.vue'
+export default {
+  components: {
+    humberger,
+    globalNav,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  height: 80px;
+  background-color: #030012;
+  padding: 12px 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  z-index: 100;
+  &__logo {
+    width: 35px;
+    height: 35px;
+  }
+  &__btn {
+    display: block;
+  }
+}
+</style>
