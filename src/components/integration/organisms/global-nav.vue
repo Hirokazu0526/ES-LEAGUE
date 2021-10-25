f7a1d1433cd10bf542908e189e00f00cb7d5ce17
Hirokazu0526 <sepak.hirokazu10@gmail.com>
2021-10-26 00:32:33 +0900

    add global-nav.vue

---
+++ src/components/integration/organisms/global-nav.vue
@@ -0,0 +1,68 @@
+<template>
+  <nav id="global_nav" class="es-League__nav">
+    <ul class="es-League__navList">
+      <li class="es-League__item">
+        <nuxt-link to="/schedule/result" class="es-League__link">
+          <img
+            class="es-League__icon"
+            src="~/assets/img/site-logo.png"
+            alt="セパタクローボール"
+          />
+          日程/結果
+        </nuxt-link>
+      </li>
+      <li class="es-League__item">
+        <nuxt-link v-scroll-to="'#live'" to class="es-League__link">
+          <img
+            class="es-League__icon"
+            src="~/assets/img/site-logo.png"
+            alt="セパタクローボール"
+          />
+          ライブ配信
+        </nuxt-link>
+      </li>
+      <li class="es-League__item">
+        <nuxt-link to="/about" class="es-League__link">
+          <img
+            class="es-League__icon"
+            src="~/assets/img/site-logo.png"
+            alt="セパタクローボール"
+          />
+          セパとは
+        </nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.es-League {
+  &__navList {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    gap: 10px;
+  }
+  &__item {
+    display: block;
+  }
+  &__link {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    text-decoration: none;
+    color: #fff;
+  }
+  &__icon {
+    width: 24px;
+    height: 24px;
+    margin-bottom: 5px;
+  }
+}
+</style>
