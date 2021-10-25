d86dd2ee6b2b488318e8d4ebd58f58f2cd963ba7
Hirokazu0526 <sepak.hirokazu10@gmail.com>
2021-10-26 00:36:49 +0900

    add pages/news/index.vue

--- src/pages/news/index.vue
+++ src/pages/news/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>news</div>
+  <section>
+    <ul>
+      <li><nuxt-link to="/news">news1</nuxt-link></li>
+      <li><nuxt-link to="/news">news2</nuxt-link></li>
+      <li><nuxt-link to="/news">news3</nuxt-link></li>
+      <li><nuxt-link to="/news">news4</nuxt-link></li>
+      <li><nuxt-link to="/news">news5</nuxt-link></li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+li {
+  list-style: none;
+  a {
+    text-decoration: none;
+  }
+}
+</style>
