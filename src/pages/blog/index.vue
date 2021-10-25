18f97d3d0ca2eb85dea9478c3ec701a612e3a926
Hirokazu0526 <sepak.hirokazu10@gmail.com>
2021-10-26 00:35:50 +0900

    add pages/blog.vue

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>blog</div>
+  <section>
+    <ul>
+      <li><nuxt-link to="/blog">blog1</nuxt-link></li>
+      <li><nuxt-link to="/blog">blog2</nuxt-link></li>
+      <li><nuxt-link to="/blog">blog3</nuxt-link></li>
+      <li><nuxt-link to="/blog">blog4</nuxt-link></li>
+      <li><nuxt-link to="/blog">blog5</nuxt-link></li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+li {
+  list-style: none;
+  a {
+    text-decoration: none;
+  }
+}
+</style>
