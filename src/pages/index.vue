822efcbc24b898e50900ea8726ce535a3af621f0
Hirokazu0526 <sepak.hirokazu10@gmail.com>
2021-10-26 00:35:36 +0900

    add pages/index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div>
+  <div class="es-League">
+    <div class="es-League__profileLink">
       <nuxt-link to="/profile">profile</nuxt-link>
     </div>
 
@@ -17,14 +17,26 @@
         </li>
       </ul>
     </div>
+    <div class="block">
+      <p id="live">Live</p>
+      <nuxt-link v-scroll-to="{ el: '#logo', offset: -30 }" to>
+        <span>Topへ移動</span>
+      </nuxt-link>
+    </div>
     <div v-if="isDay6">trueです</div>
+
+    <info-list />
+    <sponsor />
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import infoList from '~/components/integration/organisms/infoList.vue'
+import Sponsor from '~/components/integration/organisms/sponsor.vue'
 
 export default {
+  components: { infoList, Sponsor },
   async asyncData({ $microcms }) {
     // const [playersList, teamList] = await Promise.all([
     //   $microcms.get({ endpoint: 'player-details' }),
@@ -34,21 +46,43 @@ export default {
     const teamList = await $microcms.get({ endpoint: 'team-details' })
     return { teamList: teamList.contents }
   },
+  data() {
+    return {}
+  },
   computed: {
     ...mapGetters(['isDay6']),
   },
-  // created() {
-  //   this.$store.dispatch('getPlayersList')
-  // },
+  watch: {},
+  created() {},
   mounted() {},
   methods: {},
 }
 </script>
 
 <style scoped lang="scss">
+.es-League {
+  margin: 0 16px;
+  color: #fff;
+  &__profileLink {
+    margin: 10px 0;
+    a {
+      padding: 5px;
+      color: #fff;
+      background-color: dimgray;
+    }
+  }
+}
 .esleague-playerList {
   &__item {
+    color: white;
     list-style: none;
+  }
+}
+.block {
+  height: 200px;
+  background-color: rgb(211, 164, 164);
+  p {
+    padding-top: 100px;
   }
 }
 </style>
