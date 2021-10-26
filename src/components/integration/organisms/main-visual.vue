dec49a1c55c92b7c059d50e525f23418cc188687
h6k2 <sepak.hirokazu10@gmail.com>
2021-10-26 23:08:21 +0900

    add main-visual.vue

---
+++ src/components/integration/organisms/main-visual.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="es-Laegue-mainVisual">
+    <img
+      src="~/assets/img/kv001.png"
+      alt=""
+      class="es-Laegue-mainVisual__img"
+    />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.es-Laegue-mainVisual {
+  &__img {
+    width: 100%;
+  }
+}
+</style>
