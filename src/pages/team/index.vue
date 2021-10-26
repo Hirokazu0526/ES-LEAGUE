ebeec023d79350548ff58caff861a9d978d6461c
h6k2 <sepak.hirokazu10@gmail.com>
2021-10-27 00:55:57 +0900

    コード修正

--- src/pages/team/index.vue
+++ src/pages/team/index.vue
@@ -1,9 +1,33 @@
 <template>
-  <div>team</div>
+  <div>
+    <ul class="es-league-playerList">
+      <li
+        v-for="list in teamList"
+        :key="list.id"
+        class="es-league-playerList__item"
+      >
+        <p>{{ list.teamName }}</p>
+        <div v-html="list.text">></div>
+        <br />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $microcms }) {
+    const teamList = await $microcms.get({ endpoint: 'team-details' })
+    return { teamList: teamList.contents }
+  },
+}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.es-league-playerList {
+  &__item {
+    color: white;
+    list-style: none;
+  }
+}
+</style>
