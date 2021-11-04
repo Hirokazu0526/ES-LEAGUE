d472c5e5ef383f6760fbd48fb9891b99bb72ad5f
h6k2 <sepak.hirokazu10@gmail.com>
2021-11-05 00:45:12 +0900

    nuxtServerInit() 追加、state/mutations/getters 追加

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,10 @@ export const state = () => ({
   currentTime: Date.now(),
   TZ: process.env.TZ || 'Asia/Tokyo',
   route: {},
+  newsLists: [],
+  blogLists: [],
   playerLists: [],
+  teamLists: [],
   });
   
   export const mutations = {
@@ -15,18 +18,62 @@ export const state = () => ({
     setRoute(state, value) {
       state.route = value
     },
+    setNewsLists(state, newsLists) {
+      state.newsLists = newsLists
+      for (const item of state.newsLists) {
+        const itemDate = moment(item.date).format('YYYY年MM月DD日')
+        item.date = itemDate
+      }
+      
+    },
+    setBlogLists(state, blogLists) {
+      state.blogLists = blogLists
+      for (const item of state.blogLists) {
+        const itemDate = moment(item.date).format('YYYY年MM月DD日')
+        item.date = itemDate
+      }
+    },
     setPlayerLists(state, playerLists) {
-      state.playerLists = playerLists.contents
+      state.playerLists = playerLists
+      for (const item of state.playerLists) {
+        const itemDate = moment(item.birth).format('YYYY年MM月DD日')
+        item.date = itemDate
+      }
+    },
+    setTeamLists(state, teamLists) {
+      state.teamLists = teamLists
+      for (const item of state.teamLists) {
+        const itemDate = moment(item.date).format('YYYY年MM月DD日')
+        item.date = itemDate
+      }
     }
     
   }
   export const actions = {
-    async getPlayersList({commit, $microcms}) {
-      const playerLists = await $microcms.get({
-        endoPoint: 'team-details'
-      })
-      commit('setPlayerLists', playerLists)
-    },
+    // async getTeamLists({commit}, {$microcms}) {
+    //   const newsLists = await $microcms.get({
+    //     endoPoint: 'news'
+    //   })
+    //   return commit('setNewsLists', newsLists)
+    // },
+    async nuxtServerInit({ commit }, { $config }) {
+      const resNews = await this.$axios.$get(`${$config.apiUrl}news`, {
+       headers: { "X-API-KEY": $config.apiKey }
+      });
+      const resBlogs = await this.$axios.$get(`${$config.apiUrl}blog`, {
+       headers: { "X-API-KEY": $config.apiKey }
+      });
+      const resPlayers = await this.$axios.$get(`${$config.apiUrl}player-details`, {
+       headers: { "X-API-KEY": $config.apiKey }
+      });
+      const resTeams = await this.$axios.$get(`${$config.apiUrl}team-details`, {
+       headers: { "X-API-KEY": $config.apiKey }
+      });
+      commit("setNewsLists", resNews.contents);
+      commit("setBlogLists", resBlogs.contents);
+      commit("setPlayerLists", resPlayers.contents);
+      commit("setTeamLists", resTeams.contents);
+     }
   }
 
   export const getters = {
@@ -34,7 +81,16 @@ export const state = () => ({
       const now = state.currentTime
       return [20].includes(moment.tz(now, state.TZ || TZ).date())
     },
-    getPlayersList(state) {
+    getNewsList(state) {
+      return state.newsLists
+    },
+    getBlogList(state) {
+      return state.blogLists
+    },
+    getPlayerList(state) {
       return state.playerLists
+    },
+    getTeamList(state) {
+      return state.teamLists
     }
   }
