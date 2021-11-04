1e62457f213f1ff4a1144d1e1fb6544c4d3abc3c
h6k2 <sepak.hirokazu10@gmail.com>
2021-11-04 13:17:50 +0900

    データ参照先をstoreに修正

--- src/components/integration/organisms/blog.vue
+++ src/components/integration/organisms/blog.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="es-League-blog">
     <ul class="es-League-blog__list">
-      <li v-for="item in items" :key="item.id" class="es-League-blog__item">
-        <nuxt-link :to="item.url">
+      <li v-for="item in blogLists" :key="item.id" class="es-League-blog__item">
+        <nuxt-link :to="`/blog/${item.id}`">
           <div class="es-League-blog__title">
             <span>{{ item.date }}</span>
-            <h3>{{ item.title }}</h3>
+            <h3>{{ item.blogTitle }}</h3>
           </div>
         </nuxt-link>
       </li>
@@ -14,42 +14,16 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   data() {
-    return {
-      items: [
-        {
-          id: 1,
-          date: '2021年12月15日',
-          title: '最新のブログ1',
-          url: '/blog',
-        },
-        {
-          id: 2,
-          date: '2021年12月15日',
-          title: '最新のブログ2',
-          url: '/blog',
-        },
-        {
-          id: 3,
-          date: '2021年12月15日',
-          title: '最新のブログ3',
-          url: '/blog',
-        },
-        {
-          id: 4,
-          date: '2021年12月15日',
-          title: '最新のブログ4',
-          url: '/blog',
-        },
-        {
-          id: 5,
-          date: '2021年12月15日',
-          title: '最新のブログ5',
-          url: '/blog',
-        },
-      ],
-    }
+    return {}
+  },
+  computed: {
+    ...mapGetters(['getBlogList']),
+    blogLists() {
+      return this.getBlogList.slice(0, 5)
+    },
   },
 }
 </script>
