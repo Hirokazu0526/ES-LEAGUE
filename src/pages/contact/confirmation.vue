85b9767b5dccfdf596447fb1e349dd28b503e909
h6k2 <sepak.hirokazu10@gmail.com>
2021-11-24 19:19:40 +0900

    確認画面レイアウト 修正

--- src/pages/contact/confirmation.vue
+++ src/pages/contact/confirmation.vue
@@ -1,14 +1,29 @@
 <template>
-  <div style="color: #fff">
-    {{ userName }} <br />
-    {{ userKana }} <br />
-    {{ userPhoneNumber }} <br />
-    {{ userEmail }} <br />
-    {{ userContent }} <br />
-    <br />
-    <!-- <input value="もどる" type="button" @click="back" /> -->
-    <button @click="back">戻る</button>
-    <button type="submit" @click.prevent="formSubmit">送信</button>
+  <div class="es-League-confirmation" style="color: #fff">
+    <h2 class="es-League-confirmation__title">お問い合わせ</h2>
+    <div class="es-League-confirmation__container">
+      <div class="es-League-confirmation__contents">
+        <h3 class="es-League-confirmation__subject">お名前</h3>
+        <p class="es-League-confirmation__content">{{ userName }}</p>
+        <h3 class="es-League-confirmation__subject">ふりがな</h3>
+        <p class="es-League-confirmation__content">{{ userKana }}</p>
+        <h3 class="es-League-confirmation__subject">電話番号</h3>
+        <p class="es-League-confirmation__content">{{ userPhoneNumber }}</p>
+        <h3 class="es-League-confirmation__subject">メールアドレス</h3>
+        <p class="es-League-confirmation__content">{{ userEmail }}</p>
+        <h3 class="es-League-confirmation__subject">お問い合わせ内容</h3>
+        <p class="es-League-confirmation__content">{{ userContent }}</p>
+      </div>
+
+      <button class="submitButton gold" @click="back">戻る</button>
+      <button
+        class="submitButton navy"
+        type="submit"
+        @click.prevent="formSubmit"
+      >
+        送信
+      </button>
+    </div>
   </div>
 </template>
 
@@ -16,13 +31,6 @@
 import { mapState } from 'vuex'
 
 export default {
-  props: {
-    historyState: {
-      type: Object,
-      required: false,
-      default: () => {},
-    },
-  },
   computed: {
     ...mapState([
       'userName',
@@ -65,4 +73,73 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.es-League-confirmation {
+  margin: 0px 16px;
+  padding: 95px 0px;
+
+  &__title {
+    margin: 21px 0 45px;
+    text-align: center;
+    font-size: 24px;
+    position: relative;
+    color: #fff;
+    &:after {
+      content: '';
+      width: 144px;
+      height: 2px;
+      background-color: #fff;
+      display: block;
+      position: absolute;
+      bottom: -10px;
+      left: 0px;
+      right: 0px;
+      margin: 0 auto;
+    }
+  }
+
+  &__container {
+    color: #000;
+    text-align: left;
+    background: linear-gradient(-45deg, transparent 27px, #fff 20px);
+    background-position: bottom right;
+    background-size: 100%;
+    background-repeat: no-repeat;
+    padding: 30px 16px 40px;
+  }
+  &__contents {
+    margin-bottom: 30px;
+  }
+  &__subject {
+    font-size: 18px;
+  }
+  &__content {
+    font-size: 18px;
+    margin-top: 10px;
+    margin-bottom: 15px;
+  }
+}
+
+.submitButton {
+  display: block;
+  text-align: center;
+  width: 150px;
+  height: 38px;
+  color: #fff;
+  font-weight: bold;
+  font-size: 14px;
+  padding: 10px 15px 10px 0;
+  background-position: bottom right;
+  background-size: 100%;
+  background-repeat: no-repeat;
+  border: none;
+  margin: auto;
+  &.gold {
+    background: linear-gradient(-45deg, transparent 27px, #a37b30 20px);
+  }
+  &.navy {
+    margin-top: 15px;
+    background: linear-gradient(-45deg, transparent 27px, #011e43 20px);
+  }
+}
+</style>
