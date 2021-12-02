25cfebbf5df39edaaece2454adf4355782f9f488
h6k2 <sepak.hirokazu10@gmail.com>
2021-12-03 00:26:59 +0900

    add pv.vue

---
+++ src/components/integration/organisms/pv.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="es-League-pv">
+    <h3 class="es-League-pv__title">OFFICAL PV<br /></h3>
+    <div class="es-League-pv__info">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/xdNQ7SD4Eqk"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.es-League-pv {
+  margin: 30px 0;
+  padding: 20px 16px 0;
+  &__title {
+    text-align: center;
+    font-size: 32px;
+    color: #fff;
+  }
+  &__info {
+    margin-top: 15px;
+    width: 100%;
+    padding-bottom: 56.25%;
+    height: 0px;
+    position: relative;
+    & iframe {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+  &__btn {
+    margin: 15px auto 0;
+  }
+}
+</style>
