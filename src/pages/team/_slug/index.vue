97dc86cbae31e5ee675d94078918fdb9e51125f2
h6k2 <sepak.hirokazu10@gmail.com>
2021-12-30 20:08:26 +0900

    team詳細修正

--- src/pages/team/_slug/index.vue
+++ src/pages/team/_slug/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="es-League-team">
-    <h2 class="es-League-team__title">{{ teamName }}</h2>
+    <h2 class="es-League-team__title">TEAM</h2>
 
-    <img :src="image.url" :alt="teamName" class="es-League-team__uniform" />
     <div class="es-League-team__info">
-      <p class="es-League-team__text sponsor">スポンサー：{{ sponsor }}</p>
+      <img :src="image.url" :alt="teamName" class="es-League-team__uniform" />
+      <p class="es-League-team__teamName">{{ teamName }}</p>
       <p class="es-League-team__text slogan">スローガン：{{ slogan }}</p>
       <p class="es-League-team__text teamcolor">
         チームカラー：{{ teamcolor }}
@@ -12,22 +12,46 @@
       <p class="es-League-team__text teamIntroduction">
         チーム紹介：<br />{{ text }}
       </p>
-      <div>
-        <ul>
-          <li v-for="member in temaMember" :key="member.id">
-            <nuxt-link :to="`/profile/${member.id}`">
-              <img :src="member.image.url" alt="" />
-              <p>{{ member.name }}</p>
+      <div class="es-League-team__memberInfo">
+        <h3 class="es-League-team__infoTitle">MEMBER</h3>
+        <ul class="es-League-team__memberList">
+          <li
+            v-for="member in temaMember"
+            :key="member.id"
+            class="es-League-team__memberItem"
+          >
+            <nuxt-link
+              :to="`/profile/${member.id}`"
+              class="es-League-team__link"
+            >
+              <img
+                :src="member.imageSquare.url"
+                :alt="member.name"
+                class="es-League-team__memberImg"
+              />
+              <p class="es-League-team__memberName">{{ member.name }}</p>
             </nuxt-link>
           </li>
         </ul>
+        <h3 class="es-League-team__infoTitle">SPONSOR</h3>
+        <div
+          class="es-League-team__sponsorArea"
+          :class="{ bvd: sponsor === 'BVD' }"
+        >
+          <img
+            :src="sponsorLogo.url"
+            :alt="sponsor"
+            class="es-League-team__sponsorImg"
+          />
+        </div>
       </div>
     </div>
 
+    <button @click="$router.go(-1)">戻る</button>
     <es-button url="/team" :border-radius="8" class="es-League-team__btn team"
       >チーム一覧へ</es-button
     >
-    <es-button url="/profile" class="es-League-team__btn player"
+    <es-button url="/profile" color="navy" class="es-League-team__btn player"
       >選手一覧へ</es-button
     >
   </div>
@@ -62,7 +86,7 @@ export default {
   margin: 0px 16px;
   padding: 95px 0px;
   text-align: center;
-  color: #fff;
+  color: #000;
 
   &__title {
     margin: 21px 0 45px;
@@ -71,7 +95,7 @@ export default {
     position: relative;
     &:after {
       content: '';
-      width: 120px;
+      width: 159px;
       height: 2px;
       background-color: #fff;
       display: block;
@@ -82,6 +106,15 @@ export default {
       margin: 0 auto;
     }
   }
+  &__uniform {
+    margin: 0 auto;
+    display: block;
+  }
+  &__teamName {
+    font-size: 45px;
+    text-align: center;
+    margin: 10px 0;
+  }
   &__info {
     padding: 0px 16px;
     text-align: left;
@@ -90,6 +123,48 @@ export default {
     font-size: 18px;
     margin: 10px 0;
     line-height: 1.5;
+  }
+  &__memberInfo {
+    margin: 30px 0 50px;
+  }
+  &__infoTitle {
+    font-size: 30px;
+    text-align: center;
+  }
+  &__memberList {
+    list-style: none;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-content: center;
+    align-items: center;
+    gap: 15px;
+    margin: 15px 0 30px;
+  }
+
+  &__memberItem {
+    margin: 0 auto;
+  }
+  &__link {
+    color: #000;
+    text-decoration: none;
+  }
+  &__memberImg {
+    width: 110px;
+  }
+  &__memberName {
+    margin-top: 10px;
+    text-align: center;
+  }
+  &__sponsorArea {
+    width: 310px;
+    height: 78px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 15px auto;
+    &.bvd {
+      height: 110px;
+    }
   }
   &__btn {
     margin: 15px auto 0;
