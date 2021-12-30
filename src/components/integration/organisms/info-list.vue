e2b29d5f45b28b123eadc4268c31ca2438585e64
h6k2 <sepak.hirokazu10@gmail.com>
2021-12-30 20:00:07 +0900

    お知らせタブ修正

--- src/components/integration/organisms/info-list.vue
+++ src/components/integration/organisms/info-list.vue
@@ -1,37 +1,40 @@
 <template>
   <div class="es-League-info">
     <h2 class="es-League-info__title">
-      NEWS<br />
+      INFOMATION<br />
       <span>お知らせ</span>
     </h2>
     <div class="es-League-info__wrapper">
-      <tab-list
+      <!-- <tab-list
         :tab="tab"
         class="es-League-info__tab"
         @onClickTab="updateTabContents"
-      />
+      /> -->
       <div class="es-League-info__tabContents">
-        <news v-if="tab === 'news'" />
-        <blog v-else-if="tab === 'blog'" />
-        <etc v-else-if="tab === 'etc'" />
+        <news />
+        <!-- <news v-if="tab === 'news'" /> -->
+        <!-- <blog v-else-if="tab === 'blog'" /> -->
+        <!-- <etc v-else-if="tab === 'etc'" /> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import tabList from '~/components/integration/organisms/tab-list.vue'
+// import tabList from '~/components/integration/organisms/tab-list.vue'
 import news from '~/components/integration/organisms/news.vue'
-import blog from '~/components/integration/organisms/blog.vue'
-import etc from '~/components/integration/organisms/etc.vue'
+// import blog from '~/components/integration/organisms/blog.vue'
+// import etc from '~/components/integration/organisms/etc.vue'
 
-const TAB_QUERY_LIST = ['news', 'blog', 'etc']
+// const TAB_QUERY_LIST = ['news', 'blog', 'etc']
+const TAB_QUERY_LIST = ['news']
 
 export default {
-  components: { tabList, news, blog, etc },
+  // components: { tabList, news, blog, etc },
+  components: { news },
   data() {
     return {
-      tab: 'news',
+      // tab: 'news',
     }
   },
   watch: {
@@ -63,12 +66,12 @@ export default {
 
 <style lang="scss" scoped>
 .es-League-info {
-  padding: 40px 0;
   background-color: #fff;
   &__title {
     text-align: center;
     font-size: 32px;
-    color: #030012;
+    color: #000;
+    font-family: 'HNewYork';
     & span {
       margin-top: 5px;
       display: block;
@@ -78,6 +81,7 @@ export default {
   }
   &__wrapper {
     margin: 20px 16px;
+    min-height: 386px;
   }
   &__tabContents {
     background-color: #8b8b8b;
