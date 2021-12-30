194defcac099d3d76e92db34410a939e08de8091
h6k2 <sepak.hirokazu10@gmail.com>
2021-12-30 20:05:03 +0900

    お知らせ修正

--- src/pages/news/index.vue
+++ src/pages/news/index.vue
@@ -1,13 +1,16 @@
 <template>
   <section class="es-League-news">
-    <h2 class="es-League-news__title">お知らせ</h2>
+    <h2 class="es-League-news__title">
+      INFOMATION<br />
+      <span>お知らせ</span>
+    </h2>
     <ul class="es-League-news__list">
       <li
         v-for="item in getNewsList"
         :key="item.id"
         class="es-League-news__item"
       >
-        <nuxt-link :to="`/news/${item.id}`">
+        <nuxt-link :to="`/news/${item.categoryId[0]}/${item.id}`">
           <span class="es-League-news__itemDate">{{ item.date }}</span>
           <p class="es-League-news__itemTitle">{{ item.newsTitle }}</p>
         </nuxt-link>
@@ -33,22 +36,16 @@ export default {
 .es-League-news {
   padding: 95px 0px 40px;
   &__title {
-    margin: 21px 0 45px;
+    margin: 20px 0;
     text-align: center;
     font-size: 32px;
-    position: relative;
-    color: #fff;
-    &:after {
-      content: '';
-      width: 120px;
-      height: 2px;
-      background-color: #fff;
+    color: #000;
+    font-family: 'HNewYork';
+    & span {
+      margin-top: 5px;
       display: block;
-      position: absolute;
-      bottom: -10px;
-      left: 0px;
-      right: 0px;
-      margin: 0 auto;
+      font-size: 18px;
+      color: #030012;
     }
   }
   &__list {
