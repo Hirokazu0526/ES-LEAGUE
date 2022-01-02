54e50aeb07b3a312d1e160525d1a2ab95b0a4f6e
h6k2 <sepak.hirokazu10@gmail.com>
2022-01-02 16:56:11 +0900

    タブ削除

--- src/components/integration/organisms/info-list.vue
+++ src/components/integration/organisms/info-list.vue
@@ -5,61 +5,20 @@
       <span>お知らせ</span>
     </h2>
     <div class="es-League-info__wrapper">
-      <!-- <tab-list
-        :tab="tab"
-        class="es-League-info__tab"
-        @onClickTab="updateTabContents"
-      /> -->
       <div class="es-League-info__contents">
         <news />
-        <!-- <news v-if="tab === 'news'" /> -->
-        <!-- <blog v-else-if="tab === 'blog'" /> -->
-        <!-- <etc v-else-if="tab === 'etc'" /> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// import tabList from '~/components/integration/organisms/tab-list.vue'
 import news from '~/components/integration/organisms/news.vue'
-// import blog from '~/components/integration/organisms/blog.vue'
-// import etc from '~/components/integration/organisms/etc.vue'
-
-// const TAB_QUERY_LIST = ['news', 'blog', 'etc']
-const TAB_QUERY_LIST = ['news']
 
 export default {
-  // components: { tabList, news, blog, etc },
   components: { news },
   data() {
-    return {
-      // tab: 'news',
-    }
-  },
-  watch: {
-    $route(after, before) {
-      const tabQuery = after.query.tab
-      if (TAB_QUERY_LIST.find((el) => el === tabQuery)) {
-        this.tab = tabQuery
-      }
-    },
-  },
-  created() {
-    const tab = this.$route.query.tab
-    if (TAB_QUERY_LIST.find((el) => el === tab)) {
-      this.tab = tab
-    }
-  },
-  mounted() {},
-  methods: {
-    updateTabContents(selectedTab) {
-      this.tab = selectedTab
-      this.$router.push({
-        ...this.$route.query,
-        tab: selectedTab,
-      })
-    },
+    return {}
   },
 }
 </script>
