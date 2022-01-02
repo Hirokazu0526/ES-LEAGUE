c0e11a8d12b7295630af374d13309408e9dee31c
h6k2 <sepak.hirokazu10@gmail.com>
2022-01-02 14:10:44 +0900

    newslist 修正

--- src/pages/news/index.vue
+++ src/pages/news/index.vue
@@ -11,8 +11,24 @@
         class="es-League-news__item"
       >
         <nuxt-link :to="`/news/${item.categoryId[0]}/${item.id}`">
-          <span class="es-League-news__itemDate">{{ item.date }}</span>
-          <p class="es-League-news__itemTitle">{{ item.newsTitle }}</p>
+          <div class="es-League-news__itemWrapper">
+            <img
+              src="~/assets/img/news-logo.png"
+              class="es-League-news__itemImage"
+              alt=""
+            />
+            <div class="es-League-news__dateInfo">
+              <p class="es-League-news__date">
+                {{ item.date
+                }}<span class="es-League-news__category">{{
+                  item.category[0]
+                }}</span>
+              </p>
+
+              <h3 class="es-League-news__itemTitle">{{ item.newsTitle }}</h3>
+            </div>
+            <link-arrow class="es-League-news__linkArrow" />
+          </div>
         </nuxt-link>
       </li>
     </ul>
@@ -24,8 +40,10 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import linkArrow from '~/components/integration/atoms/link-arrow.vue'
 
 export default {
+  components: { linkArrow },
   computed: {
     ...mapGetters(['getNewsList']),
   },
@@ -51,26 +69,49 @@ export default {
   &__list {
     margin: 0 16px;
   }
+  &__itemWrapper {
+    display: grid;
+    grid-template-columns: 64px 1fr 20px;
+    gap: 7px;
+  }
   &__item {
     list-style: none;
-    padding: 8px 0 8px 16px;
-    background-color: #bababa;
-    &:nth-child(2n) {
-      background-color: #fff;
-    }
+    padding: 5px 10px;
+    line-height: 30px;
+    background-color: #eeeeee;
+    margin-bottom: 2px;
     a {
       text-decoration: none;
-      color: #344152;
+      color: #030012;
     }
   }
-  &__itemDate {
+  &__itemImage {
+    width: 64px;
+    height: 64px;
+  }
+  &__dateInfo {
+    padding-top: 5px;
+  }
+  &__date {
     font-size: 12px;
-    font-weight: bold;
+    margin-bottom: 10px;
   }
   &__itemTitle {
-    font-size: 15px;
-    font-weight: bold;
-    margin-top: 5px;
+    font-size: 12px;
+    font-weight: normal;
+    line-height: 12px;
+    word-break: break-all;
+  }
+  &__category {
+    background-color: #fff;
+    color: #a37b30;
+    padding: 3px 8px;
+    border: 1px solid #a37b30;
+    font-size: 12px;
+    margin-left: 5px;
+  }
+  &__linkArrow {
+    margin: auto 0;
   }
   &__btn {
     margin: 40px auto 0;
