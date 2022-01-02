10cc8e752b2d31f429a62c76f1b14a0ba9bf33e8
h6k2 <sepak.hirokazu10@gmail.com>
2022-01-02 23:32:50 +0900

    no message

--- src/plugins/global.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import VueScrollTo from 'vue-scrollto'
-import EsButton from '~/components/integration/atoms/es-button.vue'
-
-Vue.use(VueScrollTo)
-Vue.component('EsButton', EsButton)
