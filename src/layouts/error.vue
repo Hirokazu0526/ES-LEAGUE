e5e234e90f6771ebd0f82d22ab8a37246e3b145e
h6k2 <sepak.hirokazu10@gmail.com>
2022-01-02 16:56:25 +0900

    エラーページ作成

---
+++ src/layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <nuxt-error :error="error"></nuxt-error>
+  </div>
+</template>
+
+<script>
+import NuxtError from '~/components/integration/organisms/nuxt-error.vue'
+
+export default {
+  components: { NuxtError },
+  props: {
+    error: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+}
+</script>
