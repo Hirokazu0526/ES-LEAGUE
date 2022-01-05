5217c7594668a30751593a1f42c551d3f0098cfe
h6k2 <sepak.hirokazu10@gmail.com>
2022-01-05 23:58:47 +0900

    コンポーネント登録

---
+++ src/components/integration/atoms/back-button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="button" @click="$router.go(-1)">戻る</button>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  width: 160px;
+  height: 38px;
+  padding: 0;
+  color: white;
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+  text-decoration: none;
+  background-color: #666666;
+  border-radius: 2px;
+  position: relative;
+  &::after {
+    position: absolute;
+    content: '';
+    width: 4px;
+    height: 13px;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    left: 8px;
+    background: url(~/assets/img/arrow-left.svg);
+  }
+}
+</style>
--- src/plugins/global.js
+++ src/plugins/global.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueScrollTo from 'vue-scrollto'
 import EsButton from '~/components/integration/atoms/es-button.vue'
+import BackButton from '~/components/integration/atoms/back-button.vue'
 
 Vue.use(VueScrollTo)
 Vue.component('EsButton', EsButton)
+Vue.component('BackButton', BackButton)
