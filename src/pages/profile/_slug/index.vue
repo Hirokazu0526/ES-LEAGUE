92d0685e5cdb0e940b6ee05ae9b4f29ae54c17e8
h6k2 <sepak.hirokazu10@gmail.com>
2022-01-05 23:59:24 +0900

    選手詳細レイアウト修正

--- src/pages/profile/_slug/index.vue
+++ src/pages/profile/_slug/index.vue
@@ -1,32 +1,85 @@
 <template>
   <div class="es-League-profile">
-    <h2 class="es-League-profile__title">PLAYERS</h2>
+    <h2 class="es-League-profile__title">
+      PLAYER DETAIL <br /><span>選手詳細</span>
+    </h2>
 
-    <img :src="imageSquare.url" :alt="name" class="es-League-profile__name" />
-    <h1 class="es-League-profile__name">{{ name }}</h1>
+    <div class="es-League-profile__visualWrapper">
+      <img
+        :src="imageSilhouette.url"
+        :alt="name"
+        class="es-League-profile__image"
+      />
+      <div class="es-League-profile__nameWrapper">
+        <h1 class="es-League-profile__name">{{ name }}</h1>
+        <p class="es-League-profile__englishName">{{ english }}</p>
+      </div>
+      <div class="es-League-profile__positionWrapper">
+        <p
+          class="es-League-profile__uniformNumber"
+          :class="`${team.teamcolor}`"
+        >
+          {{ number }}
+        </p>
+        <p class="es-League-profile__position">{{ position[0] }}</p>
+      </div>
+    </div>
     <div class="es-League-profile__info">
-      <p class="es-League-profile__text position">
-        ポジション：{{ position[0] }}
-      </p>
-      <p class="es-League-profile__text number">背番号：{{ number }}</p>
-      <p class="es-League-profile__text birth">誕生日：{{ formatData }}</p>
-      <p class="es-League-profile__text birthplace">出身地：{{ birthplace }}</p>
-      <p class="es-League-profile__text playstyle">
-        プレースタイル：{{ playstyle }}
-      </p>
-      <p class="es-League-profile__text respect">尊敬する人：{{ respect }}</p>
-      <p class="es-League-profile__text reason">
-        セパタクローを始めたきっかけ：<br />{{ reason }}
-      </p>
+      <h3 class="es-League-profile__sectionTitle">PROFILE</h3>
+      <div class="es-League-profile__profileList">
+        <p class="es-League-profile__textTitle">誕生日</p>
+        <p class="es-League-profile__text">{{ formatData }}</p>
+      </div>
+      <div class="es-League-profile__profileList">
+        <p class="es-League-profile__textTitle">出身地</p>
+        <p class="es-League-profile__text">{{ birthplace }}</p>
+      </div>
+      <div class="es-League-profile__profileList">
+        <p class="es-League-profile__textTitle">出身大学</p>
+        <p class="es-League-profile__text">{{ university }}</p>
+      </div>
+      <div class="es-League-profile__profileList">
+        <p class="es-League-profile__textTitle">スタイル</p>
+        <p class="es-League-profile__text">{{ playstyle }}</p>
+      </div>
+      <div class="es-League-profile__profileList">
+        <p class="es-League-profile__textTitle">尊敬する人</p>
+        <p class="es-League-profile__text">{{ respect }}</p>
+      </div>
+      <div class="es-League-profile__profileList">
+        <p class="es-League-profile__textTitle">競技を始めた理由</p>
+        <p class="es-League-profile__text">{{ reason }}</p>
+      </div>
+      <div class="es-League-profile__profileList">
+        <p class="es-League-profile__textTitle">戦歴</p>
+        <p class="es-League-profile__text history">{{ history }}</p>
+        <!-- <p class="es-League-profile__text history">{{ formatHistory }}</p> -->
+      </div>
+    </div>
+    <div v-if="false" class="es-League-profile__info">
+      <h3 class="es-League-profile__sectionTitle">PERSONAL DATA</h3>
+      <div class="es-League-profile__profileList">
+        <p class="es-League-profile__textTitle">レシーブ成功率</p>
+        <p class="es-League-profile__text">75%</p>
+      </div>
+      <div class="es-League-profile__profileList">
+        <p class="es-League-profile__textTitle">サービス成功率</p>
+        <p class="es-League-profile__text">75%</p>
+      </div>
+      <div class="es-League-profile__profileList">
+        <p class="es-League-profile__textTitle">サービスエース</p>
+        <p class="es-League-profile__text">75%</p>
+      </div>
+      <div class="es-League-profile__profileList">
+        <p class="es-League-profile__textTitle">アタックレシーブ</p>
+        <p class="es-League-profile__text">75%</p>
+      </div>
     </div>
 
-    <button @click="$router.go(-1)">戻る</button>
-    <es-button :url="`/team/${team.id}`" class="es-League-profile__btn team"
-      >チーム情報</es-button
-    >
-    <es-button url="/" color="navy" class="es-League-profile__btn team"
-      >TOPへ</es-button
-    >
+    <div class="es-League-profile__btnWrapper">
+      <back-button />
+      <es-button url="/team" class="es-League-team__btn">チーム一覧</es-button>
+    </div>
   </div>
 </template>
 
@@ -50,19 +103,9 @@ export default {
     formatData() {
       return moment(this.birth).format('YYYY年M月D日')
     },
-  },
-  mounted() {
-    this.setAnimation()
-  },
-  methods: {
-    setAnimation() {
-      this.$anime({
-        targets: '.es-League-profile__ability',
-        scale: 1.5,
-        duration: 2000,
-        easing: 'easeInOutQuad',
-      })
-    },
+    // formatHistory() {
+    //   return this.history.split(' ').join('\n')
+    // },
   },
 }
 </script>
@@ -75,69 +118,136 @@ export default {
   color: #000;
 
   &__title {
-    margin: 21px 0 45px;
+    margin: 24px 0;
     text-align: center;
     font-size: 32px;
-    position: relative;
-    &:after {
-      content: '';
-      width: 120px;
-      height: 2px;
-      background-color: #fff;
+    color: #000000;
+    font-family: 'HNewYork';
+    & span {
+      margin-top: 5px;
       display: block;
-      position: absolute;
-      bottom: -10px;
-      left: 0px;
-      right: 0px;
-      margin: 0 auto;
+      font-size: 18px;
+      font-family: '游ゴシック', 'Yu Gothic', '游ゴシック体', YuGothic,
+        sans-serif;
     }
   }
-  &__ability {
-    background-image: url(~/assets/img/pentagon.svg);
-    background-repeat: no-repeat;
-    background-size: 100%;
-    width: 53px;
-    height: 51px;
-    display: block;
-    margin: 10px auto;
+
+  &__visualWrapper {
+    position: relative;
+    margin: 24px 0 0;
+    height: 365px;
   }
-  &__info {
-    margin: 40px 0 0;
-    padding: 0px 16px;
+
+  &__image {
+    position: absolute;
+    width: 160px;
+    right: 16px;
+  }
+
+  &__nameWrapper {
+    position: absolute;
+    top: 40px;
+    left: 16px;
     text-align: left;
   }
-  &__text {
+  &__name {
+    font-size: 30px;
+    font-weight: bold;
+    margin-bottom: 4px;
+  }
+  &__englishName {
     font-size: 18px;
-    margin: 10px 0;
+    font-weight: normal;
+  }
+  &__positionWrapper {
+    position: absolute;
+    bottom: 40px;
+    left: 16px;
+  }
+  &__uniformNumber {
+    font-size: 80px;
+    font-family: 'HNewYork';
+    &.green {
+      color: green;
+    }
+    &.red {
+      color: #8b0000;
+    }
+    &.blue {
+      color: blue;
+    }
+    &.black {
+      color: black;
+    }
+    &.yellow {
+      color: yellow;
+    }
+    &.orange {
+      color: orange;
+    }
+    &.pink {
+      color: pink;
+    }
+    &.skyblue {
+      color: skyblue;
+    }
+  }
+  &__position {
+    font-size: 20px;
+    font-family: 'HNewYork';
+  }
+
+  &__info {
+    margin-bottom: 40px;
+  }
+
+  &__sectionTitle {
+    font-size: 22px;
+    padding: 10px 0;
+    text-align: center;
+    border-top: 2px solid #000;
+    border-bottom: 2px solid #000;
+  }
+  &__profileList {
+    display: grid;
+    grid-template-columns: 112px 1fr;
+    column-gap: 21px;
+    align-items: center;
+    justify-content: center;
+    padding: 10px 16px;
+    font-size: 24px;
+    font-weight: bold;
+    border-bottom: 1px dashed #000;
+    &:last-child {
+      border-bottom: none;
+    }
+    span {
+      font-weight: normal;
+    }
+  }
+  &__textTitle {
+    font-size: 14px;
+    font-weight: bold;
+    text-align: left;
+  }
+
+  &__text {
+    font-size: 14px;
+    font-weight: normal;
+    text-align: left;
+    line-height: 1.4;
+    &.history {
+      white-space: break-spaces;
+    }
   }
   &__btn {
     margin: 15px auto 0;
   }
-  &__back {
-    text-align: center;
-    width: 150px;
-    a {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 100%;
-      height: 38px;
-      position: relative;
-      margin-left: 10px;
-      padding: 0 15px 0 0;
-      color: white;
-      font-size: 14px;
-      font-weight: bold;
-      text-align: center;
-      text-decoration: none;
-      background: linear-gradient(-45deg, transparent 27px, #a37b30 20px);
-      background-position: bottom right;
-      background-size: 100%;
-      background-repeat: no-repeat;
-      &:hover {
-        cursor: pointer;
-      }
-    }
+  &__btnWrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 16px;
+    justify-items: center;
   }
 }
 </style>
