9850582d5672462b7ec2c3f7388eae8dd0450b12
h6k2 <sepak.hirokazu10@gmail.com>
2022-01-17 16:40:52 +0900

    add about-es.vue

--- src/pages/esleague/index.vue
+++ src/pages/esleague/index.vue
@@ -1,9 +1,304 @@
 <template>
-  <div>about esleague</div>
+  <div class="es-League-aboutEs">
+    <h2 class="es-League-aboutEs__title">
+      ABOUT ES LEAGUE<br /><span>ESリーグについて</span>
+    </h2>
+    <div class="es-League-aboutEs__wrapper">
+      <div class="es-League-aboutEs__story">
+        <p class="es-League-aboutEs__storyText">
+          セパタクロー競技にとって最高峰の世界大会は<br />
+          4年に一度開催されるアジア競技大会である<br />
+          その最高峰の戦いが2026年、<br />
+          日本は愛知県名古屋市にて開催が決まった<br />
+          <br />
+          日本セパタクロー界の最高成績は<br />
+          2018年 アジア競技大会での銀メダル<br />
+          金メダルにはあと一歩届いていない<br />
+          <br />
+          パンデミックな時代だからといって<br />
+          ただ立ち止まって待っていても<br />
+          金メダルが我々に手を差し伸べてくれるわけもなく<br />
+          自分たちの力で勝ち取りに行かなければならない<br />
+          <br />
+          今ここから新たな歴史の一歩を踏み出す<br />
+          全ては2026年愛知名古屋アジア競技大会で<br />
+          センターポールに日の丸を掲げるために<br />
+          そしてその先の未来に向かって
+        </p>
+      </div>
+      <section class="es-League-aboutEs__section">
+        <h3 class="es-League-aboutEs__title">Mission<br /><span>使命</span></h3>
+        <p class="es-League-aboutEs__sectionText">
+          ES LEAGUEは<br />
+          「する」「観る」「支える」の様々な機会を創出し<br />
+          セパタクローに関わる全ての人を幸せにします
+        </p>
+      </section>
+      <section class="es-League-aboutEs__section">
+        <h3 class="es-League-aboutEs__title">
+          League Identitiy<br /><span>名称とロゴに込めた想い</span>
+        </h3>
+        <img
+          src="~/assets/img/aboutES/about-es-logo.svg"
+          alt="ES LEAGUE LOGO"
+          class="es-League-aboutEs__logo"
+        />
+        <p class="es-League-aboutEs__sectionText left">
+          ES LEAGUEはExtreme SepakTakraw
+          Leagueの略称です。この名称には「空中の格闘技」とも表されるセパタクローが持つ、「過激で極度の足技が人々を魅了する」想いが込められています。<br /><br />
+          また、ロゴは大切にする３つのステークホルダーである競技者・観戦者・支援者への想いを込めたストライプにより、セパタクローのダイナミックさを表現し、本リーグのデザインアイデンティティとしています。
+        </p>
+      </section>
+      <section class="es-League-aboutEs__section">
+        <h3 class="es-League-aboutEs__title">
+          Regulation<br /><span>試合概要</span>
+        </h3>
+        <table class="es-League-aboutEs__table">
+          <tbody>
+            <tr class="es-League-aboutEs__tableRow">
+              <td class="es-League-aboutEs__tableTitle">チーム数</td>
+              <td class="es-League-aboutEs__tableData">
+                男子5チーム、女子3チームのリーグ戦
+              </td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableTitle">登録選手</td>
+              <td class="es-League-aboutEs__tableData">1チーム最大5名</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableTitle" rowspan="4">種 目</td>
+              <td class="es-League-aboutEs__tableData bottom">【男子】</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableData regulation">
+                レグ(3人制)、ダブル(2人制)、クワッド(4人制)
+              </td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableData bottom top">【女子】</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableData regulation">
+                レグ(3人制)
+              </td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableTitle">セット数</td>
+              <td class="es-League-aboutEs__tableData">
+                1セット21点マッチ<br /><span>(最大25点)</span>
+              </td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableTitle">シーズン</td>
+              <td class="es-League-aboutEs__tableData">
+                2022年2月〜7月<br /><span
+                  >(国際大会等で中断する可能性あり)</span
+                >
+              </td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableTitle" rowspan="6">
+                試合形式
+              </td>
+              <td class="es-League-aboutEs__tableData bottom">
+                ＜1試合 3セットゲーム＞
+              </td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableData regulation">
+                【男子】<br />
+                &emsp;ダブル：1セットマッチ<br />
+                &emsp;&emsp;レグ：1セットマッチ<br />クワッド：1セットマッチ
+              </td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableData bottom top">【女子】</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableData regulation">
+                &emsp;&emsp;レグ：3セットマッチ(2セット先取)
+              </td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableData regulation bottom">
+                ＜メンバー登録＞
+              </td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableData regulation">
+                試合開始前に各種目に出場するメンバー表を提出。また、必ず一人1種目は出場することとする
+              </td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableTitle">配点</td>
+              <td class="es-League-aboutEs__tableData">
+                勝ち = 2ポイント<br />負け = 0ポイント
+              </td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableTitle">順位</td>
+              <td class="es-League-aboutEs__tableData">
+                勝ち点の合計が大きい順に順位を決定。<br />勝ち点が同じ場合は、得失セット数の大きいチームが上位となる。得失セット数が同じ場合は当該チームの勝敗により決定する。
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+      <section class="es-League-aboutEs__section">
+        <h3 class="es-League-aboutEs__title">
+          Organization<br /><span>組織概要</span>
+        </h3>
+        <table class="es-League-aboutEs__table">
+          <tbody>
+            <tr class="es-League-aboutEs__tableRow">
+              <td class="es-League-aboutEs__tableTitle">法人名称</td>
+              <td class="es-League-aboutEs__tableData">
+                一般社団法人ES LEAGUE
+              </td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableTitle">代表理事</td>
+              <td class="es-League-aboutEs__tableData">寺島 武志</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableTitle" rowspan="3">理事</td>
+              <td class="es-League-aboutEs__tableData">飯田 義隆</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableData">小林 裕和</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableData">中川 憲</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableTitle">テキスト</td>
+              <td class="es-League-aboutEs__tableData">テキスト</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableTitle">テキスト</td>
+              <td class="es-League-aboutEs__tableData">テキスト</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableTitle" rowspan="4">
+                テキスト
+              </td>
+              <td class="es-League-aboutEs__tableData">テキスト</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableData">テキスト</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableData">テキスト</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableData">テキスト</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableTitle">テキスト</td>
+              <td class="es-League-aboutEs__tableData">テキスト</td>
+            </tr>
+            <tr>
+              <td class="es-League-aboutEs__tableTitle">テキスト</td>
+              <td class="es-League-aboutEs__tableData">テキスト</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </div>
+    <es-button url="/" arrow="right" class="es-League-aboutEs__btn"
+      >TOPに戻る</es-button
+    >
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.es-League-aboutEs {
+  margin: 0px 16px;
+  padding: 80px 0px 40px;
+  text-align: center;
+  color: #000;
+
+  &__title {
+    margin: 24px 0;
+    text-align: center;
+    font-size: 32px;
+    color: #000000;
+    font-family: 'HNewYork';
+    & span {
+      margin-top: 5px;
+      display: block;
+      font-size: 18px;
+      font-family: '游ゴシック', 'Yu Gothic', '游ゴシック体', YuGothic,
+        sans-serif;
+    }
+  }
+  &__wrapper {
+    margin-bottom: 30px;
+  }
+  &__story {
+    background: url(~/assets/img/aboutES/about-es-kv.png) 50% 50% no-repeat;
+    background-size: 100%;
+    margin: 0 calc(50% - 50vw);
+    width: 100vw;
+    height: 563px;
+  }
+  &__storyText {
+    margin: 0px 16px;
+    padding: 16px 0;
+    text-align: center;
+    font-size: 13px;
+    font-weight: normal;
+    line-height: 1.6;
+  }
+  &__section {
+    margin-top: 36px;
+    margin-bottom: 43px;
+  }
+  &__sectionText {
+    font-size: 14px;
+    font-weight: normal;
+    line-height: 1.6;
+    &.left {
+      text-align: left;
+      padding: 0 16px;
+    }
+  }
+  &__logo {
+    margin-bottom: 16px;
+    width: 100%;
+  }
+  &__tableTitle {
+    background-color: #f2f1f0;
+    min-width: 120px;
+    vertical-align: middle;
+    font-size: 14px;
+    font-weight: bold;
+  }
+  &__tableData {
+    text-align: left;
+    vertical-align: middle;
+    font-size: 14px;
+    line-height: 1.4;
+    padding: 8px 5px;
+    &.bottom {
+      padding-bottom: 0;
+      &.top {
+        padding-top: 0;
+      }
+    }
+    &.regulation {
+      padding-top: 0;
+    }
+    & span {
+      font-size: 13px;
+    }
+  }
+  &__btn {
+    margin: 16px auto 0;
+  }
+}
+</style>
