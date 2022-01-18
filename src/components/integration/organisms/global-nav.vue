07f6eef8159729c6510b3216bf3de092d0314e9c
h6k2 <sepak.hirokazu10@gmail.com>
2022-01-18 10:54:55 +0900

    pc時ナビ項目追加

--- src/components/integration/organisms/global-nav.vue
+++ src/components/integration/organisms/global-nav.vue
@@ -1,6 +1,7 @@
 <template>
   <nav id="global_nav" class="es-League__nav">
-    <ul class="es-League__navList">
+    <!-- sp用グローバルナビ -->
+    <ul class="es-League__navList sp">
       <li class="es-League__item">
         <nuxt-link to="/schedule/result" class="es-League__link">
           <img
@@ -33,6 +34,70 @@
         </nuxt-link>
       </li>
     </ul>
+    <!-- pc用グローバルナビ -->
+    <ul class="es-League__navList pc">
+      <li class="es-League__item">
+        <nuxt-link to="/schedule/result" class="es-League__link">
+          <img
+            class="es-League__icon"
+            src="~/assets/img/header/calendar.svg"
+            alt="カレンダー"
+          />
+          日程・結果
+        </nuxt-link>
+      </li>
+      <li class="es-League__item">
+        <nuxt-link to="/rank" class="es-League__link">
+          <img
+            class="es-League__icon"
+            src="~/assets/img/header/calendar.svg"
+            alt="カレンダー"
+          />
+          順位表
+        </nuxt-link>
+      </li>
+      <li class="es-League__item">
+        <!-- <nuxt-link v-scroll-to="'#live'" to="/#live" class="es-League__link"> -->
+        <nuxt-link to="/team" class="es-League__link">
+          <img
+            class="es-League__icon"
+            src="~/assets/img/header/shorts.svg"
+            alt="ユニフォーム"
+          />
+          チーム紹介
+        </nuxt-link>
+      </li>
+      <li class="es-League__item">
+        <nuxt-link to="/esleague" class="es-League__link">
+          <img
+            class="es-League__icon"
+            src="~/assets/img/header/site-logo.svg"
+            alt="セパタクローボール"
+          />
+          ES リーグについて
+        </nuxt-link>
+      </li>
+      <li class="es-League__item">
+        <nuxt-link to="/news" class="es-League__link">
+          <img
+            class="es-League__icon"
+            src="~/assets/img/header/site-logo.svg"
+            alt="セパタクローボール"
+          />
+          NEWS
+        </nuxt-link>
+      </li>
+      <li class="es-League__item">
+        <nuxt-link to="/about" class="es-League__link">
+          <img
+            class="es-League__icon"
+            src="~/assets/img/header/site-logo.svg"
+            alt="セパタクローボール"
+          />
+          セパタクローとは
+        </nuxt-link>
+      </li>
+    </ul>
   </nav>
 </template>
 
@@ -48,6 +113,9 @@ export default {}
     gap: 20px;
     justify-items: stretch;
     align-items: center;
+    &.pc {
+      display: none;
+    }
   }
   &__item {
     display: block;
@@ -67,6 +135,19 @@ export default {}
     width: 24px;
     height: 24px;
     margin-bottom: 5px;
+  }
+}
+@media screen and (min-width: 768px) {
+  .es-League {
+    &__navList {
+      &.sp {
+        display: none;
+      }
+      &.pc {
+        display: grid;
+        grid-template-columns: 65px 50px 65px 120px 50px 107px;
+      }
+    }
   }
 }
 @media screen and (max-width: 374px) {
