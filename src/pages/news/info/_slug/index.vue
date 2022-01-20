16473cdda2bdd1e8046c467a3dfcba2ba3906edf
h6k2 <sepak.hirokazu10@gmail.com>
2022-01-21 01:26:43 +0900

    カテゴリー追加

--- src/pages/news/info/_slug/index.vue
+++ src/pages/news/info/_slug/index.vue
@@ -5,7 +5,9 @@
       <span>お知らせ</span>
     </h2>
     <div class="es-League-news__contanier">
-      <p class="es-League-news__date">{{ formatData }}</p>
+      <p class="es-League-news__date">
+        {{ formatData }}<span>{{ category[0] }}</span>
+      </p>
       <h3 class="es-League-news__newsTitle">{{ newsTitle }}</h3>
       <!-- eslint-disable-next-line vue/no-v-html -->
       <div class="es-League-news__newsText" v-html="newsText"></div>
@@ -74,6 +76,15 @@ export default {
   &__date {
     text-align: left;
     font-size: 12px;
+    span {
+      display: inline-block;
+      margin-left: 10px;
+      background-color: #fff;
+      color: #a37b30;
+      padding: 3px 8px;
+      border: 1px solid #a37b30;
+      font-size: 12px;
+    }
   }
   &__newsTitle {
     font-size: 20px;
@@ -89,6 +100,30 @@ export default {
     grid-template-columns: 1fr 1fr;
     column-gap: 16px;
     justify-items: center;
+  }
+}
+@media screen and (min-width: 768px) {
+  .es-League-news {
+    max-width: 600px;
+    margin: 0 auto;
+    &__title {
+      font-size: 45px;
+      margin-bottom: 45px;
+      & span {
+        font-size: 24px;
+      }
+    }
+    &__date {
+      font-size: 16px;
+      span {
+        font-size: 14px;
+      }
+    }
+    ::v-deep &__newsText {
+      p {
+        line-height: 1.5;
+      }
+    }
   }
 }
 </style>
