4cb47a692fd6fe7f331730d629c2284ba1da18df
h6k2 <sepak.hirokazu10@gmail.com>
2022-01-20 17:57:01 +0900

    fix footer image

--- src/components/integration/organisms/footer.vue
+++ src/components/integration/organisms/footer.vue
@@ -1,55 +1,62 @@
 <template>
   <div class="es-League-footer">
-    <div class="es-League-footer__bg">
-      <ul class="es-League-footer__list">
-        <li class="es-League-footer__listItem">
-          <nuxt-link to="/esleague">
-            <p class="es-League-footer__listTitle">ES LEAGUEとは</p>
-          </nuxt-link>
-        </li>
-        <li class="es-League-footer__listItem">
-          <nuxt-link to="/news">
-            <p class="es-League-footer__listTitle">NEWS</p>
-          </nuxt-link>
-        </li>
-        <li class="es-League-footer__listItem">
-          <nuxt-link to="/rank">
-            <p class="es-League-footer__listTitle">順位表</p>
-          </nuxt-link>
-        </li>
-        <li class="es-League-footer__listItem">
-          <nuxt-link to="/contact">
-            <p class="es-League-footer__listTitle">お問い合わせ</p>
-          </nuxt-link>
-        </li>
-        <li class="es-League-footer__listItem">
-          <a href="http://jstaf.jp/" target="_blank">
-            <p class="es-League-footer__listTitle">
-              一般社団法人<br />日本セパタクロー協会
-            </p>
-          </a>
-        </li>
-      </ul>
-      <div class="es-League-footer__snsList">
-        <a
-          href="https://twitter.com/es_league_japan"
-          target="_blank"
-          class="es-League-footer__icon twitter"
-        ></a>
-        <a href="" class="es-League-footer__icon facebook"></a>
-        <a
-          href="https://www.instagram.com/es_league_japan/"
-          target="_blank"
-          class="es-League-footer__icon instagram"
-        ></a>
-        <a
-          href="https://www.tiktok.com/@es_league?lang=ja"
-          target="_blank"
-          class="es-League-footer__icon tiktok"
-        ></a>
-      </div>
-      <div class="es-League-footer__copyRight">
-        <small>© 2022 ES LEAGUE All Rights Reserved.</small>
+    <div class="es-League-footer__wrapper">
+      <img
+        src="~/assets/img/logo/esleague_long_white.svg"
+        class="es-League-footer__bgImg"
+        alt="ES LEAGUE LOGO"
+      />
+      <div class="es-League-footer__container">
+        <ul class="es-League-footer__list">
+          <li class="es-League-footer__listItem">
+            <nuxt-link to="/esleague">
+              <p class="es-League-footer__listTitle">ES LEAGUEとは</p>
+            </nuxt-link>
+          </li>
+          <li class="es-League-footer__listItem">
+            <nuxt-link to="/news">
+              <p class="es-League-footer__listTitle">NEWS</p>
+            </nuxt-link>
+          </li>
+          <li class="es-League-footer__listItem">
+            <nuxt-link to="/rank">
+              <p class="es-League-footer__listTitle">順位表</p>
+            </nuxt-link>
+          </li>
+          <li class="es-League-footer__listItem">
+            <nuxt-link to="/contact">
+              <p class="es-League-footer__listTitle">お問い合わせ</p>
+            </nuxt-link>
+          </li>
+          <li class="es-League-footer__listItem">
+            <a href="http://jstaf.jp/" target="_blank">
+              <p class="es-League-footer__listTitle">
+                一般社団法人<br />日本セパタクロー協会
+              </p>
+            </a>
+          </li>
+        </ul>
+        <div class="es-League-footer__snsList">
+          <a
+            href="https://twitter.com/es_league_japan"
+            target="_blank"
+            class="es-League-footer__icon twitter"
+          ></a>
+          <a href="" class="es-League-footer__icon facebook"></a>
+          <a
+            href="https://www.instagram.com/es_league_japan/"
+            target="_blank"
+            class="es-League-footer__icon instagram"
+          ></a>
+          <a
+            href="https://www.tiktok.com/@es_league?lang=ja"
+            target="_blank"
+            class="es-League-footer__icon tiktok"
+          ></a>
+        </div>
+        <div class="es-League-footer__copyRight">
+          <small>© 2022 ES LEAGUE All Rights Reserved.</small>
+        </div>
       </div>
     </div>
   </div>
@@ -67,15 +74,31 @@ export default {
 .es-League-footer {
   background-color: #000000;
   color: #fff;
+  &__wrapper {
+    position: relative;
+    padding: 20px 16px 20px 0;
+    overflow: hidden;
+    max-width: 950px;
+    margin: 0 auto;
+  }
+  &__bgImg {
+    position: absolute;
+    top: -10px;
+    left: 0;
+    width: 210px;
+  }
   &__bg {
     display: grid;
     grid-template-rows: 1fr 110px 40px;
     width: 100%;
     height: 370px;
-    background: url(~/assets/img/logo/esleague_long_white.svg) 0% -70px no-repeat;
-    background-size: 70%;
     padding: 27px 16px 20px;
   }
+
+  &__container {
+    display: grid;
+  }
+
   &__list {
     list-style: none;
   }
@@ -98,7 +121,7 @@ export default {
     display: inline-block;
   }
   &__snsList {
-    margin: 50px 0px 20px;
+    margin: 20px 0px 20px;
     text-align: right;
   }
   &__icon {
@@ -130,8 +153,43 @@ export default {
 }
 @media screen and (max-width: 374px) {
   .es-League-footer {
+    &__bgImg {
+      width: 180px;
+    }
     &__copyRight {
       font-size: 12px;
+    }
+  }
+}
+@media screen and (min-width: 768px) {
+  .es-League-footer {
+    &__wrapper {
+      padding: 40px 10px 20px 0;
+    }
+    &__bgImg {
+      top: -50px;
+    }
+    &__list {
+      padding-left: 145px;
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      justify-items: start;
+      width: 600px;
+      margin: 0 auto 0;
+      align-items: center;
+    }
+    &__listItem {
+      text-align: left;
+      &:nth-child(2n + 1) {
+        margin-left: 0px;
+      }
+    }
+    &__snsList {
+      text-align: center;
+    }
+    &__copyRight {
+      font-size: 18px;
+      text-align: center;
     }
   }
 }
