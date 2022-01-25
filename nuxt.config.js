d092705bcbdca687b6ddd057885b3bdd03c532c8
h6k2 <sepak.hirokazu10@gmail.com>
2022-01-26 00:43:31 +0900

    fix meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,15 +16,28 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'es-league',
+    title: 'ES LEAGUE 公式サイト',
     htmlAttrs: {
       lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: '日本初のセパタクロー国内リーグ ES LEAGUE 公式サイトです' || '' },
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'keywords', name: 'keywords', content: 'セパタクロー,セパタクローリーグ,エス リーグ,SEPAKTAKRAW,ES LEAGUE,日本初' },
+
+      { hid: 'og:site_name', property: 'og:site_name', content: 'ES LEAGUE' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://es-league.jp' },
+      { hid: 'og:title', property: 'og:title', content: 'ES LEAGUE 公式サイト' },
+      { hid: 'og:description', property: 'og:description', content: '日本初のセパタクロー国内リーグ ES LEAGUE 公式サイトです' },
+      { hid: 'og:image', property: 'og:image', content: '~/assets/img/ogp/ogp.png' },
+      { hid: 'og:locale', content: 'ja_JP' },
+
+      // { hid: 'fb:app_id', property: 'fb:app_id', content: 'App-ID' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@ES_LEAGUE_Japan' }
     ],
     link: [
       { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' },
