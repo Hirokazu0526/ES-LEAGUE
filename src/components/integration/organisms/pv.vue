c618fc3e9c4a651b8c2d38fac4a914123decf482
h6k2 <sepak.hirokazu10@gmail.com>
2022-01-26 23:25:18 +0900

    fix pv loop setting

--- src/components/integration/organisms/pv.vue
+++ src/components/integration/organisms/pv.vue
@@ -5,7 +5,7 @@
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/xdNQ7SD4Eqk"
+        src="https://www.youtube.com/embed/xdNQ7SD4Eqk?loop=1&playlist=xdNQ7SD4Eqk"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
