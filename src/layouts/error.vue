112623e530ba3a5041a430206a9c1149cc697a52
h6k2 <sepak.hirokazu10@gmail.com>
2022-01-26 16:09:31 +0900

    fix error.vue

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <p>お探しのページは存在しません</p>
+  <div class="error">
+    <p v-if="error.statusCode === 404">お探しのページは存在しません</p>
+    <p v-else>エラーが発生しました</p>
     <es-button url="/" arrow="right" color="gray">TOPへ戻る</es-button>
     <!-- <p>ステータスコード：{{ error.statusCode }}</p>
     <p>メッセージ：{{ error.message }}</p> -->
@@ -12,10 +13,14 @@ export default {
   props: {
     error: {
       type: Object,
-      default() {
-        return {}
-      },
+      default: null,
     },
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.error {
+  min-height: 600px;
+}
+</style>
