2a8821d0a526fbd95fca1e93d1409b91d8ac69b6
h6k2 <sepak.hirokazu10@gmail.com>
2022-02-24 22:21:08 +0900

    saraya pv 追加

--- src/pages/movie/index.vue
+++ src/pages/movie/index.vue
@@ -10,7 +10,7 @@
           <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/KEPl2b2gkG8"
+            src="https://www.youtube.com/embed/EKeycdY0-ww"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
