bf4da813e8e89ad9db68aa3a0159da8459d4315c
Hirokazu kobayashi <sepak.hirokazu10@gmail.com>
2023-07-08 18:10:07 +0700

    middlewear追加＆コメントアウト

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,6 +50,7 @@ export default {
   },
   router: {
     base: baseDir,
+    // middleware: 'maintenance'
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -167,5 +168,5 @@ export default {
 
   generate: {
     fallback: true,
-  }
+  },
 }
