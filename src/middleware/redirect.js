1ba27cd1b4f980ad1aa6bedab787980c5efc05e4
Hirokazu kobayashi <sepak.hirokazu10@gmail.com>
2023-08-03 00:07:32 +0900

    middleware修正

--- src/middleware/redirect.js
+++ src/middleware/redirect.js
@@ -1,5 +1,5 @@
 export default function ({ route, redirect }) {
-  if (route.path  !== '/redirect')  {
-    return redirect('/redirect');
+  if (route.path  === '/spectating' || route.path  === '/nagoya' )  {
+    return redirect('/');
   }
 }
